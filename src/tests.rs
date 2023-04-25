01e6cc72c4292610450db8912195e3662ea40f76
j.v.o.koskinen@gmail.com <j.v.o.koskinen@gmail.com>
2023-04-25 22:41:27 +0300

    Randomize the range of values in nth_small test

--- src/tests.rs
+++ src/tests.rs
@@ -27,13 +27,14 @@ fn ternary() {
 
 #[test]
 fn nth_small() {
-    let repeat = 1000;
-    let count = 20;
-    let mut rng = usize::rng(0).in_range(0, count);
+    let repeat = 10000;
+    let count = 600;
+    let mut range_rng = usize::rng(0).in_range(1, count);
 
     for _iter in 0..repeat {
-        let mut data: Vec<_> = rng.by_ref().take(count).collect();
-        let k = rng.get();
+        let rng = usize::rng(0).in_range(0, range_rng.get());
+        let mut data: Vec<_> = rng.take(count).collect();
+        let k = range_rng.get();
         let (u, v) = select_nth_small(&mut data, k);
         let uth = data[u];
         let vth = data[v];
