2323b9635c562c3130e8ed49436301d493df6e7b
j.v.o.koskinen@gmail.com <j.v.o.koskinen@gmail.com>
2023-05-01 13:15:40 +0300

    Improve the example

--- examples/large/main.rs
+++ examples/large/main.rs
@@ -1,6 +1,6 @@
-use std::time::Instant;
+use std::{hint::black_box, time::Instant};
 
-use select::{adaptive_quickselect, floyd_rivest_select_nth};
+use select::{select_nth_unstable};
 
 mod pcg_rng;
 use pcg_rng::PCGRng;
@@ -14,37 +14,76 @@ fn random_u32s(count: usize) -> Vec<u32> {
     data
 }
 
+/// Run the provided closure `repeat` times, timing each run and printing the
+/// elapsed time for each run.
+fn timeit<F: FnMut()>(mut f: F, repeat: usize) {
+    let mut times = Vec::with_capacity(repeat);
+    for _ in 0..repeat {
+        let now = Instant::now();
+        black_box(f());
+        let elapsed = now.elapsed().as_secs_f32();
+        times.push(elapsed);
+    }
+    let avg = times.iter().sum::<f32>() / times.len() as f32;
+    let min = times
+        .iter()
+        .min_by(|a, b| a.partial_cmp(b).unwrap())
+        .unwrap();
+    let max = times
+        .iter()
+        .max_by(|a, b| a.partial_cmp(b).unwrap())
+        .unwrap();
+    eprintln!(
+        "  {repeat} runs: avg {avg} seconds, min {min} seconds, max {max} seconds",
+        repeat = repeat,
+        avg = avg,
+        min = min,
+        max = max,
+    );
+}
+
 fn main() {
     eprint!("Generating data...");
-    let master = random_u32s(u32::MAX as usize);
+    let count = 10_000_000;
+    let repeat = 1000;
+    let master = random_u32s(count);
     eprintln!("{} elements", master.len());
 
-    eprintln!("Selecting 42nd element...");
+    eprintln!("Selecting the 42nd element using the Floyd & Rivest algorithm ...");
 
-    {
-        let mut data = master.clone();
-        let now = Instant::now();
-        floyd_rivest_select_nth(data.as_mut_slice(), 42);
-        let elapsed = now.elapsed().as_secs_f32();
-        let found = data[42];
-        eprintln!("  Floyd & Rivest select_nth: {found} in {elapsed} seconds");
-    }
+    timeit(
+        || {
+            let mut data = master.clone();
+            select_nth_unstable(data.as_mut_slice(), 42);
+        },
+        repeat,
+    );
 
-    {
-        let mut data = master.clone();
-        let now = Instant::now();
-        adaptive_quickselect(data.as_mut_slice(), 42);
-        let elapsed = now.elapsed().as_secs_f32();
-        let found = data[42];
-        eprintln!("  Adaptive quickselect: {found} in {elapsed} seconds");
-    }
+    eprintln!("Selecting the 42nd element using std::slice::select_nth_unstable ...");
+    timeit(
+        || {
+            let mut data = master.clone();
+            data.select_nth_unstable(42);
+        },
+        repeat,
+    );
 
-    {
-        let mut data = master.clone();
-        let now = Instant::now();
-        data.select_nth_unstable(42);
-        let elapsed = now.elapsed().as_secs_f32();
-        let found = data[42];
-        eprintln!("  std::slice::select_nth_unstable: {found} in {elapsed} seconds");
-    }
+    let mid = master.len() / 2;
+    eprintln!("Selecting the median element using the Floyd & Rivest algorithm ...");
+    timeit(
+        || {
+            let mut data = master.clone();
+            select_nth_unstable(data.as_mut_slice(), mid);
+        },
+        repeat,
+    );
+
+    eprintln!("Selecting the median element using std::slice::select_nth_unstable ...");
+    timeit(
+        || {
+            let mut data = master.clone();
+            data.select_nth_unstable(mid);
+        },
+        repeat,
+    );
 }
