10cf19e838f6ba4ecaec8f30689f020413d8ebd4
j.v.o.koskinen@gmail.com <j.v.o.koskinen@gmail.com>
2023-07-13 10:42:36 +0300

    Add test for pivot selection

--- src/tests.rs
+++ src/tests.rs
@@ -1,120 +1,13 @@
 #[cfg(feature = "std")]
 extern crate std;
 
-use std::vec::Vec;
+use std::{io::Write, println, vec::Vec};
 
 use crate::{
-    partition_equal_max, partition_equal_min, turboselect, sample, select_nth_unstable, sort::sort_at,
-    wyrand::WyRng,
+    choose_pivot, partition_equal_max, partition_equal_min, sample, select_nth_unstable,
+    sort::sort_at, turboselect, wyrand::WyRng,
 };
 
-fn iter_rng(rng: &mut WyRng, count: usize, high: usize) -> impl Iterator<Item = usize> + '_ {
-    core::iter::from_fn(move || Some(rng.bounded_usize(0, high))).take(count)
-}
-
-fn shuffle<T>(data: &mut [T], rng: &mut WyRng) {
-    let len = data.len();
-    for i in 0..len - 1 {
-        let j = rng.bounded_usize(i, len);
-        data.swap(i, j);
-    }
-}
-
-#[test]
-fn large_median() {
-    let mut rng = WyRng::new(123);
-
-    #[cfg(not(miri))]
-    let count = 10_000_000;
-    #[cfg(miri)]
-    let count = 1000;
-
-    let mid = count / 2;
-
-    let mut data: Vec<usize> = (0..count).collect();
-    shuffle(data.as_mut_slice(), &mut rng);
-    let (left, median, right) = select_nth_unstable(data.as_mut_slice(), mid);
-    assert_eq!(median, &mid);
-    assert!(left.iter().all(|elem| elem < &mid));
-    assert!(right.iter().all(|elem| elem > &mid));
-}
-
-#[test]
-fn sawtooth() {
-    let mut rng = WyRng::new(123);
-
-    #[cfg(not(miri))]
-    let count = 10_000;
-    #[cfg(miri)]
-    let count = 1000;
-
-    #[cfg(not(miri))]
-    let repeat = 1000;
-
-    #[cfg(miri)]
-    let repeat = 10;
-
-    /// Returns a vector of `u32`s with a sawtooth pattern.
-    fn sawtooth_u32s(count: usize, rng: &mut WyRng) -> Vec<u32> {
-        let mut data = Vec::with_capacity(count);
-        let count = count as u32;
-        let max_lenght = (count as f64).sqrt() as u32;
-        let length = rng.bounded_u32(max_lenght / 4 + 1, max_lenght);
-        for index in 0..count {
-            let x = index % length;
-            data.push(x);
-        }
-        data
-    }
-
-    for iter in 0..repeat {
-        let index = (iter * count) / repeat;
-        let mut data = sawtooth_u32s(count, rng.as_mut());
-        let (left, nth, right) = select_nth_unstable(data.as_mut_slice(), index);
-        assert!(left.iter().all(|elem| elem <= nth));
-        assert!(right.iter().all(|elem| elem >= nth));
-    }
-}
-
-#[test]
-fn reversed() {
-    let mut rng = WyRng::new(123);
-
-    #[cfg(not(miri))]
-    let count = 10_000;
-    #[cfg(miri)]
-    let count = 1000;
-
-    #[cfg(not(miri))]
-    let repeat = 1000;
-
-    #[cfg(miri)]
-    let repeat = 10;
-
-    /// Returns a vector of integers in the range `0..count`, in reversed order.
-    fn reversed_u32s(count: usize, rng: &mut WyRng) -> Vec<u32> {
-        let mut data = Vec::with_capacity(count);
-        let max = rng.bounded_u32(0, count as u32);
-        for index in 0..count {
-            data.push((max * (count - index + 1) as u32) / (count as u32));
-        }
-        data
-    }
-
-    for iter in 0..repeat {
-        let index = (iter * count) / repeat;
-        let mut data = reversed_u32s(count, rng.as_mut());
-        let (left, nth, right) = select_nth_unstable(data.as_mut_slice(), index);
-        left.iter().enumerate().for_each(|(i, elem)| match i {
-            i if elem > nth => panic!("iter {iter}: left[{i}] = {elem} > nth = {nth}"),
-            _ => (),
-        });
-        right.iter().enumerate().for_each(|(i, elem)| match i {
-            i if elem < nth => panic!("iter {iter}: left[{i}] = {elem} < nth = {nth}"),
-            _ => (),
-        });
-    }
-}
 #[test]
 fn bool_median() {
     #[cfg(not(miri))]
@@ -151,19 +44,107 @@ fn extreme_index() {
     let index = 42;
 
     let mut data: Vec<usize> = (0..count).collect();
-    shuffle(data.as_mut_slice(), &mut rng);
-    let (left, &mut nth, right) = select_nth_unstable(data.as_mut_slice(), index);
+    shuffle(&mut data, &mut rng);
+    let (left, &mut nth, right) = select_nth_unstable(&mut data, index);
 
     assert!(left.iter().all(|elem| elem < &nth));
     assert!(right.iter().all(|elem| elem > &nth));
     assert_eq!(nth, data[index]);
 
     let index = count - 42;
-    let (left, &mut nth, right) = select_nth_unstable(data.as_mut_slice(), index);
+    let (left, &mut nth, right) = select_nth_unstable(&mut data, index);
 
     assert!(left.iter().all(|elem| elem < &nth));
     assert!(right.iter().all(|elem| elem > &nth));
     assert_eq!(nth, data[index]);
+}
+
+fn iter_rng(rng: &mut WyRng, count: usize, high: usize) -> impl Iterator<Item = usize> + '_ {
+    core::iter::from_fn(move || Some(rng.bounded_usize(0, high))).take(count)
+}
+
+#[test]
+fn large_median() {
+    let mut rng = WyRng::new(123);
+
+    #[cfg(not(miri))]
+    let count = 10_000_000;
+    #[cfg(miri)]
+    let count = 1000;
+
+    let mid = count / 2;
+
+    let mut data: Vec<usize> = (0..count).collect();
+    shuffle(&mut data, &mut rng);
+    let (left, median, right) = select_nth_unstable(&mut data, mid);
+    assert_eq!(median, &mid);
+    assert!(left.iter().all(|elem| elem < &mid));
+    assert!(right.iter().all(|elem| elem > &mid));
+}
+
+#[test]
+fn min_10() {
+    let len = 10;
+    let mut rng = WyRng::new(123);
+
+    #[cfg(not(miri))]
+    let repeat = 1000;
+    #[cfg(miri)]
+    let repeat = 10;
+
+    for _iter in 0..repeat {
+        let mut data: Vec<_> = iter_rng(rng.as_mut(), len, len / 2).collect();
+        let mut cloned = data.clone();
+        let (_u, v) = partition_equal_min(&mut data, 0);
+        let min = &data[0];
+        for (i, elem) in data.iter().enumerate() {
+            if i <= v {
+                assert!(elem == min);
+            } else {
+                assert!(elem > min);
+            }
+        }
+        data.sort();
+        cloned.sort();
+        assert_eq!(data, cloned);
+    }
+
+    let mut data: Vec<_> = core::iter::repeat(1).take(10).collect();
+    let (u, v) = partition_equal_min(&mut data, 0);
+    assert_eq!(u, 0);
+    assert_eq!(v, 9);
+}
+#[test]
+fn max_10() {
+    let len = 10;
+    let mut rng = WyRng::new(123);
+
+    #[cfg(not(miri))]
+    let repeat = 10000;
+    #[cfg(miri)]
+    let repeat = 10;
+
+    for _iter in 0..repeat {
+        let mut data: Vec<_> = iter_rng(rng.as_mut(), len, len / 2).collect();
+        let mut cloned = data.clone();
+        let (u, _v) = partition_equal_max(&mut data, 0);
+        let max = &data[u];
+        for (i, elem) in data.iter().enumerate() {
+            if i >= u {
+                assert!(elem == max);
+            } else {
+                assert!(elem < max);
+            }
+        }
+        data.sort();
+        cloned.sort();
+        assert_eq!(data, cloned);
+    }
+
+    let mut data: Vec<_> = core::iter::repeat(1).take(10).collect();
+    let (u, v) = partition_equal_max(data.as_mut_slice(), 0);
+    assert_eq!(u, 0);
+    assert_eq!(v, 9);
 }
 
 #[test]
@@ -220,6 +201,78 @@ fn nth_small() {
 }
 
 #[test]
+#[ignore]
+fn pivots() {
+    let mut rng = WyRng::new(123);
+    let count = 10_000;
+    let repeat = 10_000;
+
+    let mut output = std::fs::File::create("misc/pivots.csv").unwrap();
+    let mut results = Vec::new();
+    let mut good_pivots = 0;
+    writeln!(results, "index,partition_at,fit").unwrap();
+    for _iter in 0..repeat {
+        let mut data: Vec<_> = (0..count).collect();
+        shuffle(&mut data, rng.as_mut());
+
+        let index = rng.bounded_usize(0, count);
+        let (p, _) = choose_pivot(&mut data, index, rng.as_mut());
+        let partition_at = data[p];
+        let overshoot = if index < count / 2 {
+            partition_at < index
+        } else {
+            partition_at > index
+        };
+        good_pivots += usize::from(!overshoot);
+        let fit = if overshoot { "bad" } else { "good" };
+        writeln!(results, "{index},{partition_at},{fit}").unwrap();
+    }
+    let ratio = good_pivots as f64 / repeat as f64;
+    println!("good pivots: {good_pivots}/{repeat}, ratio {ratio:.3}",);
+    output.write_all(&results).unwrap();
+}
+
+#[test]
+fn reversed() {
+    let mut rng = WyRng::new(123);
+
+    #[cfg(not(miri))]
+    let count = 10_000;
+    #[cfg(miri)]
+    let count = 1000;
+
+    #[cfg(not(miri))]
+    let repeat = 1000;
+
+    #[cfg(miri)]
+    let repeat = 10;
+
+    /// Returns a vector of integers in the range `0..count`, in reversed order.
+    fn reversed_u32s(count: usize, rng: &mut WyRng) -> Vec<u32> {
+        let mut data = Vec::with_capacity(count);
+        let max = rng.bounded_u32(0, count as u32);
+        for index in 0..count {
+            data.push((max * (count - index + 1) as u32) / (count as u32));
+        }
+        data
+    }
+
+    for iter in 0..repeat {
+        let index = (iter * count) / repeat;
+        let mut data = reversed_u32s(count, rng.as_mut());
+        let (left, nth, right) = select_nth_unstable(data.as_mut_slice(), index);
+        left.iter().enumerate().for_each(|(i, elem)| match i {
+            i if elem > nth => panic!("iter {iter}: left[{i}] = {elem} > nth = {nth}"),
+            _ => (),
+        });
+        right.iter().enumerate().for_each(|(i, elem)| match i {
+            i if elem < nth => panic!("iter {iter}: left[{i}] = {elem} < nth = {nth}"),
+            _ => (),
+        });
+    }
+}
+
+#[test]
 fn sample_n() {
     #[cfg(not(miri))]
     let repeat = 1000;
@@ -240,69 +293,48 @@ fn sample_n() {
 }
 
 #[test]
-fn min_10() {
-    let len = 10;
+fn sawtooth() {
     let mut rng = WyRng::new(123);
+
+    #[cfg(not(miri))]
+    let count = 10_000;
+    #[cfg(miri)]
+    let count = 1000;
 
     #[cfg(not(miri))]
     let repeat = 1000;
+
     #[cfg(miri)]
     let repeat = 10;
 
-    for _iter in 0..repeat {
-        let mut data: Vec<_> = iter_rng(rng.as_mut(), len, len / 2).collect();
-        let mut cloned = data.clone();
-        let (_u, v) = partition_equal_min(data.as_mut_slice(), 0);
-        let min = &data[0];
-        for (i, elem) in data.iter().enumerate() {
-            if i <= v {
-                assert!(elem == min);
-            } else {
-                assert!(elem > min);
-            }
+    /// Returns a vector of `u32`s with a sawtooth pattern.
+    fn sawtooth_u32s(count: usize, rng: &mut WyRng) -> Vec<u32> {
+        let mut data = Vec::with_capacity(count);
+        let count = count as u32;
+        let max_lenght = (count as f64).sqrt() as u32;
+        let length = rng.bounded_u32(max_lenght / 4 + 1, max_lenght);
+        for index in 0..count {
+            let x = index % length;
+            data.push(x);
         }
-        data.sort();
-        cloned.sort();
-        assert_eq!(data, cloned);
+        data
     }
 
-    let mut data: Vec<_> = core::iter::repeat(1).take(10).collect();
-    let (u, v) = partition_equal_min(data.as_mut_slice(), 0);
-    assert_eq!(u, 0);
-    assert_eq!(v, 9);
+    for iter in 0..repeat {
+        let index = (iter * count) / repeat;
+        let mut data = sawtooth_u32s(count, rng.as_mut());
+        let (left, nth, right) = select_nth_unstable(data.as_mut_slice(), index);
+        assert!(left.iter().all(|elem| elem <= nth));
+        assert!(right.iter().all(|elem| elem >= nth));
+    }
 }
 
-#[test]
-fn max_10() {
-    let len = 10;
-    let mut rng = WyRng::new(123);
-
-    #[cfg(not(miri))]
-    let repeat = 10000;
-    #[cfg(miri)]
-    let repeat = 10;
-
-    for _iter in 0..repeat {
-        let mut data: Vec<_> = iter_rng(rng.as_mut(), len, len / 2).collect();
-        let mut cloned = data.clone();
-        let (u, _v) = partition_equal_max(data.as_mut_slice(), 0);
-        let max = &data[u];
-        for (i, elem) in data.iter().enumerate() {
-            if i >= u {
-                assert!(elem == max);
-            } else {
-                assert!(elem < max);
-            }
-        }
-        data.sort();
-        cloned.sort();
-        assert_eq!(data, cloned);
+fn shuffle<T>(data: &mut [T], rng: &mut WyRng) {
+    let len = data.len();
+    for i in 0..len - 1 {
+        let j = rng.bounded_usize(i, len);
+        data.swap(i, j);
     }
-
-    let mut data: Vec<_> = core::iter::repeat(1).take(10).collect();
-    let (u, v) = partition_equal_max(data.as_mut_slice(), 0);
-    assert_eq!(u, 0);
-    assert_eq!(v, 9);
 }
 
 #[test]
